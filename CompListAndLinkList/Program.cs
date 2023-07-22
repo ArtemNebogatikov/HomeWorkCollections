fb4a422598710e5db84966bcdf1d2cb53bdf64e5
Artem Nebogatikov <a.nebogatikov@yandex.ru>
2023-07-22 18:34:17 +0400

    Add project files.

---
+++ CompListAndLinkList/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+
+namespace CompListAndLinkList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = Path.Combine(GetSolutionRoot(), "text1.txt");
+            string str;
+            var list = new List<string>();
+            using (StreamReader sr = File.OpenText(path))
+            {
+                str = sr.ReadToEnd();
+                var stopWatch = Stopwatch.StartNew();
+                list.Add(str);
+                var timeFromList = stopWatch.Elapsed.TotalMilliseconds;
+                Console.WriteLine($"Время List<T>: {timeFromList.ToString()}");
+
+                var linkedList = new LinkedList<string>();
+                stopWatch.Restart();
+                linkedList.AddLast(str);
+                var timeFromLinkList = stopWatch.Elapsed.TotalMilliseconds;
+                Console.WriteLine($"Время LinkedList<T>: {timeFromLinkList.ToString()}");
+
+                char[] arr = str.ToCharArray();
+                var listChar = new List<char>();
+                stopWatch.Restart();
+                foreach (char c in arr)
+                {
+                    listChar.Add(c);
+                }
+                var listTimeChar = stopWatch.Elapsed.TotalMilliseconds;
+                Console.WriteLine($"Время добавления массива char в List<T>: {listTimeChar}");
+                var linkChar = new LinkedList<char>();
+                stopWatch.Restart();
+                foreach(char c in arr)
+                {
+                    linkChar.AddLast(c);
+                }
+                var linkTime = stopWatch.Elapsed.TotalMilliseconds;
+                Console.WriteLine($"Время добавления массива char в LinkedList<T>: {linkTime}");
+            }
+            Console.ReadLine();
+        }
+        static string GetSolutionRoot()
+        {
+            var dir = Path.GetDirectoryName(Directory.GetCurrentDirectory());
+            var fullname = Directory.GetParent(dir).FullName;
+            var projectRoot = fullname.Substring(0, fullname.Length - 4);
+            return projectRoot;
+        }
+    }
+}
---
+++ RepeatWord/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+
+namespace RepeatWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = Path.Combine(GetSolutionRoot(), "text1.txt");
+            string str;
+            using (StreamReader sr = File.OpenText(path))
+            {
+                str = sr.ReadToEnd();
+                var noPunctuationText = new string(str.Where(c => !char.IsPunctuation(c)).ToArray());
+                var strWord = noPunctuationText.Split();
+                List<string> words = new List<string>(strWord);
+                List<string> wordWithoutSpace = new List<string>(strWord);
+                foreach (var word in words)
+                {
+                    if (word == "")
+                    {
+                        wordWithoutSpace.Remove(word);
+                    }
+                }
+                var set = new HashSet<string>();
+                var wordsRepeat = new Dictionary<string, int>();
+                int count = 1;
+                foreach (var item in wordWithoutSpace)
+                {
+                    if (!set.Add(item))
+                    {
+                        if (!wordsRepeat.ContainsKey(item))
+                        {
+                            wordsRepeat.Add(item, count + 1);
+                        }
+                        else
+                        {
+                            wordsRepeat[item]++;
+                        }
+                    }
+                }
+                foreach (var word in wordsRepeat)
+                {
+                    if(word.Value > 10) 
+                    {
+                        Console.WriteLine(word.Key + " " + word.Value);
+                    }
+                }
+                
+                           
+            }
+            Console.ReadKey();
+            static string GetSolutionRoot()
+            {
+                var dir = Path.GetDirectoryName(Directory.GetCurrentDirectory());
+                var fullname = Directory.GetParent(dir).FullName;
+                var projectRoot = fullname.Substring(0, fullname.Length - 4);
+                return projectRoot;
+            }
+        }
+    }
+}
